b399950d66810be65f364a64d487a046e39e81f7
Mike Z <mzapata2991@gmail.com>
2020-01-11 18:51:43 -0600

    seperated script into it's own js file; drink search working and random drink selection pulling in random number from list

---
+++ letsDrink.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+    //search for alcohol
+
+    $("#find-drink").on("click", function (event) {
+
+        event.preventDefault();
+
+        // Here we grab the text from the input box
+        var drink = $("#drink-input").val();
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://the-cocktail-db.p.rapidapi.com/search.php?i=" + drink,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+                "x-rapidapi-key": "666caa2618msh64e5a09c9984d6cp1d6d08jsn711c7459d6d7"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+
+            for (var i = 0; i < response.ingredients.length; i++) {
+                var drinkDiv = $("<div>");
+                var name = $("<h3>").html(response.ingredients[0].strIngredient);
+                var description = $("<p>").html(response.ingredients[0].strDescription);
+                drinkDiv.append(name, description);
+                $("#drink-view").html(drinkDiv);
+            }
+            // $("#drink-view").text(JSON.stringify(response, null, 3));
+            // $("#drink-view").text(JSON.stringify(response.ingredients[0].strDescription));
+        });
+
+    });
+
+
+    //pick random drink
+
+    $("#find-random").on("click", function (event) {
+
+        event.preventDefault();
+
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+                "x-rapidapi-key": "666caa2618msh64e5a09c9984d6cp1d6d08jsn711c7459d6d7"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            // for (var i = 0; i < response.drinks.length; i++) {
+            var randomDrink = Math.floor(Math.random() * response.drinks.length);
+            console.log(randomDrink);
+            var drinkDiv = $("<div>");
+            var name = $("<h3>").html(randomDrink.strDrink);
+            var image = $("<img>").attr("src", randomDrink.strDrinkThumb);
+            drinkDiv.append(randomDrink, name, image);
+            $("#drink-view").html(drinkDiv);
+            // }
+        });
+
+
+
+
+
+    });
+})
